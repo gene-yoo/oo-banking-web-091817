bc5cb27dda049ccce9abb39a1daf38bcf46cbf14
gene-yoo <gene.y.yoo@gmail.com>
2017-09-21 17:00:48 -0400

    Create class definitions and methods for Transfer and Bank Account

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount = 50)
+      @sender = sender
+      @receiver = receiver
+      @status = "pending"
+      @amount = amount
+  end
+
+  def valid?
+      @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+      if @sender.balance >= @amount && valid? && @status == "pending"
+          @sender.balance -= @amount
+          @receiver.balance += @amount
+          @status = "complete"
+      else
+          @status = "rejected"
+          "Transaction rejected. Please check your account balance."
+      end
+  end
+
+  def reverse_transfer
+      if @receiver.balance >= @amount && @status == "complete" && valid?
+          @sender.balance += @amount
+          @receiver.balance -= @amount
+          @status = "reversed"
+      end
+  end
 end
